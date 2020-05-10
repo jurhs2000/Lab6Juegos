8d951ce710ec1960a3db6b493261ad713dc731bd
Julio Herrera <jurhs2000@gmail.com>
2020-05-09 21:29:23 -0600

    enemy collider following animation

---
+++ Assets/Scripts/EnemyAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ https://www.raywenderlich.com/2427-unity-4-3-2d-tutorial-physics-and-screen-sizes
+ */
+public class EnemyAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private PolygonCollider2D[] colliders;
+    private int currentColliderIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetColliderForSprite(int spriteNum)
+    {
+        colliders[currentColliderIndex].enabled = false;
+        currentColliderIndex = spriteNum;
+        colliders[currentColliderIndex].enabled = true;
+    }
+}
