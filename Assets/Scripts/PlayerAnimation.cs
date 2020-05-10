b98d7dac22d596b12f99ae920d3e063410672342
Julio Herrera <jurhs2000@gmail.com>
2020-05-09 22:48:59 -0600

    movimiento agregado

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator playerAnimator;
+    private bool isInAnimation;
     void Start()
     {
-        
+        playerAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AnimationWalk()
     {
-        
+        if (!isInAnimation)
+            playerAnimator.Play("RunAnim");
+    }
+
+    public void StayAnimation()
+    {
+        if (!isInAnimation)
+            playerAnimator.Play("PrincipalAnimation");
+    }
+
+    public void jumpAnimation()
+    {
+        playerAnimator.Play("JumpAnim");
+        isInAnimation = true;
+    }
+
+    public void JumpFAnimation()
+    {
+        playerAnimator.Play("JumpFAnim");
+        isInAnimation = true;
+    }
+
+    public void setIsInAnimationFalse()
+    {
+        isInAnimation = false;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Vector2 startPosition;
+    Rigidbody2D rb2d;
+    public float maxVelocity;
+    public float jumpForce;
+    public bool isFalling;
+    private int lives;
+    private int power;
+    private bool isSuper;
+    private PlayerAnimation playerAnimation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxVelocity = 7;
+        jumpForce = 10;
+        rb2d = GetComponent<Rigidbody2D>();
+        playerAnimation = FindObjectOfType(typeof(PlayerAnimation)) as PlayerAnimation;
+        lives = 3;
+        power = 50;
+        isSuper = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump") && !isFalling)
+        {
+            Jump();
+            isFalling = true;
+        }
+        float move = Input.GetAxis("Horizontal");
+        if (move != 0)
+        {
+            if (move > 0)
+            {
+                gameObject.transform.localScale = new Vector2(5, 5);
+            } else if (move < 0)
+            {
+                gameObject.transform.localScale = new Vector2(-5, 5);
+            }
+            playerAnimation.AnimationWalk();
+            rb2d.velocity = new Vector2(move * maxVelocity, rb2d.velocity.y);
+        } else
+        {
+            playerAnimation.StayAnimation();
+        }
     }
+
+    void Jump()
+    {
+        rb2d.velocity = new Vector2(rb2d.velocity.x, 18f);
+        if (rb2d.velocity.x < 4.5f && rb2d.velocity.x > -4.5f)
+        {
+            playerAnimation.jumpAnimation();
+        } else
+        {
+            playerAnimation.JumpFAnimation();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isFalling = false;
+            playerAnimation.setIsInAnimationFalse();
+        }
+    }
+
 }
